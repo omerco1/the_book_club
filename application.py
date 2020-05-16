3b89ca7dfaa676ba2d787ea5315d82b6314a2dc8
omerco1 <31459523+omerco1@users.noreply.github.com>
2020-05-16 01:51:39 -0700

    implementing user registration page

--- application.py
+++ application.py
@@ -6,8 +6,11 @@ from flask import render_template
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Table, Date, Float, MetaData
 
 app = Flask(__name__)
+
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
@@ -20,10 +23,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 # import secrets 
 # secrets.token_urlsafe(16)
 # Note store more sensitive info in 'redis' database
-# Session["USERNAME"] = ua.username
+
 app.config["SECRET_KEY"]= 'Cza8wCCc2vKc-qHwzL5YFA'
-
-
 app.static_folder = 'static'
 Session(app)
 
@@ -33,12 +34,15 @@ db = scoped_session(sessionmaker(bind=engine))
 class userauth:
     timesloggedin = 0
     username = ''
+    auth_failed = False
 
     def check_user_or_insert(self, user, passw):
         session['username'] = user
         if (db.execute("SELECT * FROM users WHERE username = :username AND password = :password", {"username": user,"password": passw}).rowcount == 0): 
-            db.execute("INSERT INTO users (username, password, userid, timesloggedin) VALUES (:username, :password, :userid, :timesloggedin)", {"username": user, "password": passw, "userid": uuid.uuid1(),  "timesloggedin": 1})
+            db.execute("INSERT INTO users (username, password, useruuid, timesloggedin) VALUES (:username, :password, :useruuid, :timesloggedin)", {"username": user, "password": passw, "useruuid": uuid.uuid1(),  "timesloggedin": 1})
             db.commit()
+            self.timesloggedin = 1 
+            self.username = user
         else: 
             result_proxy = db.execute("SELECT * FROM users WHERE username = :username AND password = :password", {"username": user, "password": passw})
             query_dict = self.get_dict_from_resultproxy(result_proxy)
@@ -69,10 +73,11 @@ class userauth:
         return self.get_dict_from_resultproxy(rp)
 
 ua = userauth()
+blankuser = userauth()
 
 @app.route('/', methods=['GET', 'POST'])
 def my_form_post():
-    print("IN POST %s " % request.method)
+
     if request.method == 'POST':
         print("OMER IN HERE")
 
@@ -95,8 +100,8 @@ def my_form_post():
         ua.check_user_or_insert(userEmail, userPassword)
         #using session, for an already registered user: 
         #redirect(url_for('profile'))
-        return render_template('/home.html', user = ua)
-    return render_template('index.html')
+        return render_template('/login.html', user = ua)
+    return render_template('login.html', user=blankuser)
 
 @app.route("/home")
 def home():
@@ -106,7 +111,13 @@ def home():
         return render_template('home.html', user=userdata)
     else: 
         print('Username not found!')
-        return redirect(url_for('index'))
+        return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        print('attempting to register') 
+    return render_template('register.html', user=ua)
 
 @app.route("/signout")
 def sign_out(): 
@@ -130,12 +141,53 @@ def table_exists(name):
     print('Table "{}" exists: {}'.format(name, ret))
     return ret
 
-if __name__ == '__main__':
+
+
+def init_tables():
+    print('Checking/Creating tables... ')
 
     books_table_name = 'books'
+    users_table_name = 'users'
+    reviews_table_name = 'reviews'
+    local_books_file_name = 'books.csv'
+    
+    metadata = MetaData(engine)
 
-    if not table_exists(books_table_name):
-        print('Building books table...')
+    Table(users_table_name, metadata,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('username', String, nullable=False),
+    Column('password', String, nullable=False),
+    Column('useruuid', String, nullable=False),
+    Column('timesloggedin', Integer, nullable=False))
+
+    # if not table_exists(books_table_name):
+    # create table sql query (doesnt work): 
+    #db.execute("CREATE TABLE %s (bookid SERIAL NOT NULL PRIMARY KEY, title TEXT NOT NULL, isbn TEXT NOT NULL, author TEXT NOT NULL, yearpublished INT NOT NULL)" % books_table_name)
+
+    # Create a table with the appropriate Columns
+    Table(books_table_name, metadata,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('yearpublished', Integer, nullable=False),
+    Column('title', String, nullable=False),
+    Column('isbn', String, nullable=False),
+    Column('author', String, nullable=False))
+
+    Table(reviews_table_name, metadata,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('datae', Date, nullable=False),
+    Column('title', String, nullable=False),
+    Column('reviewbody', String, nullable=False),
+    Column('userid', String, nullable=False),
+    Column('numstars', String, nullable=False)
+    )
+
+    metadata.create_all() 
+
+@app.before_first_request
+def _run_on_start():
+    app.run(debug=True) 
+    init_tables()
+
+
 
     
-    app.run(debug=True) 
